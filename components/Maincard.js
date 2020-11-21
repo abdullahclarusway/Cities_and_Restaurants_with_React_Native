4edd1953ff8975c8410c87c15c0f92d040d5c294
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2020-11-21 21:02:53 +0100

    Add Components

---
+++ components/Maincard.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import {Text, View, StyleSheet, TouchableOpacity, Image, Dimensions} from 'react-native';
+// const Maincard = (props) => {
+//     return(
+//         <TouchableOpacity
+//               onPress = {()=>  props.onmyselected()}
+//               >
+//                   <Text >{props.main.name}</Text>
+//               </TouchableOpacity>
+//     )
+// };
+
+// export default Maincard;
